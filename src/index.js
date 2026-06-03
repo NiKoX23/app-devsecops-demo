07b15a05d69cb464bdd2ceac1652e3b2a142579f
Nicolas <nicox.lobosg@gmail.com>
2026-06-02 21:03:22 -0400

    cambio minimo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
     }
 
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello DevSecOps');
+    res.end('Hello DevSecOpS');
 });
 
 server.listen(3000, () => {
